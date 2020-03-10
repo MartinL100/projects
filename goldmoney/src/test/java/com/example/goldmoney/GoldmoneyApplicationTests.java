04f94f1bdee4d20d505d8974c4bd792440ffb3cb
martingcong <1246456207@qq.com>
2020-03-10 18:48:15 +0800

    jsb主页+公司注册

---
+++ goldmoney/src/test/java/com/example/goldmoney/GoldmoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goldmoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldmoneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
