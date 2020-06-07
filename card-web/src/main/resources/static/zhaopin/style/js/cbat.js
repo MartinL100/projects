f214f9b48ad6f5388cf152ff733b358077abbe67
martingcong <1246456207@qq.com>
2020-06-07 20:56:22 +0800

    挑灯招聘 version 2.0

---
+++ card-web/src/main/resources/static/zhaopin/style/js/cbat.js
@@ -0,0 +1,10 @@
+var footerhtml = "<div class=\"wrapper\">\n" +
+    "        <a href=\"javascript:void(0)\"  rel=\"nofollow\">联系电话：028-8858487</a>\n" +
+    "        <a href=\"javascript:void(0)\" >联系地址：成都市青羊区光华北三路98号</a>\n" +
+    "        <a href=\"javascript:void(0)\"  rel=\"nofollow\">QQ:327481306</a>\n" +
+    "        <div class=\"copyright\">&copy;2019-2020 tiao deng <a target=\"_blank\"\n" +
+    "                                                        href=\"http://www.beian.miit.gov.cn/icp/publish/query/icpMemoInfo_showPage.action\">蜀ICP备20013346号-1</a>\n" +
+    "        </div>\n" +
+    "    </div>";
+
+document.getElementById("footer").innerHTML=footerhtml;
